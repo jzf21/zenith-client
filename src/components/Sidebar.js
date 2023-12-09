9cb2569ba2a7fd45cadc4ca8542b56bd3fddd19c
hacksh4w <mrinalsnair@gmail.com>
2023-12-10 03:33:43 +0530

    componentise sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,18 @@
-import React from "react";
+import React from 'react';
+import ROption from './RecordingOptions';
 
-const Sidebar = ({}) => {
+const Sidebar = () => {
   return (
     <div className="h-screen w-72 flex flex-col justify-start items-center gap-4">
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
-      <div className="w-full h-20 px-4 flex flex-col justify-center items-start border-2 border-zenith-lav">
-        <p>Info Session</p>
-        <p>More info</p>
-      </div>
+      <ROption />
+      <ROption />
+      <ROption />
+      <ROption />
+      <ROption />
+      <ROption />
     </div>
   );
 };
 
 export default Sidebar;
+
