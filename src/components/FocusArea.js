455bb398411fe3650db15036a37b8b6802866efa
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-12-10 09:24:31 +0530

    dummy

--- src/components/FocusArea.js
+++ src/components/FocusArea.js
@@ -3,15 +3,40 @@ import React from "react";
 const FocusArea = ({}) => {
   return (
     <div className="h-full w-full border-2 border-zenith-lav flex flex-col justify-start items-center gap-4">
-      <div className="h-1/5">
+      <div className="h-1/5 p-14">
         <div className="text-4xl">Inteview with PM</div>
       </div>
       <div className="h-1 w-3/5 bg-zenith-lav"></div>
       <div>
         <p className="text-2xl">Interview taken during christmas holidays</p>
-        <p>
-          PM talking about the increasing gas prices and how it is affecting the
-          economy
+        <p className="p-14">
+        Interviewer: Good evening, ladies and gentlemen. Today, we have the pleasure of sitting down with Mr. Intervier, a Project Manager who has been closely monitoring the recent surge in gap prizes during the Christmas season. Mr. Doe, thank you for joining us.<br />
+
+Intervier (PM): Thank you for having me.<br />
+
+Interviewer: To start off, could you provide some context on what exactly we mean by "gap prizes"?<br />
+
+Intervier (PM): Certainly. "Gap prizes" refer to the disparities in monetary rewards or gifts during the holiday season. This can include variations in bonuses, salary increments, or even gift distributions among employees or individuals.<br />
+
+Interviewer: Interesting. So, why is this an issue, and how does it tie into the economy?<br />
+
+Intervier (PM): The increase in gap prizes, especially during Christmas, can exacerbate existing economic inequalities. When a select few receive significantly higher rewards than others, it widens the wealth gap within a community or organization. This can lead to lower employee morale, decreased productivity, and, on a larger scale, hinder overall economic growth.<br />
+
+Interviewer: How does this impact the broader economy?<br />
+
+Intervier (PM): Well, when a substantial portion of the population feels left out or unfairly compensated, it creates dissatisfaction and a sense of injustice. This can result in lower consumer spending, as individuals may be less inclined to participate in the holiday shopping spree or invest in the economy. Ultimately, it can contribute to a less stable and less prosperous economic environment.<br />
+
+Interviewer: Are there any specific measures that can be taken to address this issue?<br />
+
+Intervier (PM): Absolutely. Organizations need to be more transparent and equitable in their reward distribution policies. Implementing fair and inclusive practices can help bridge the gap and promote a healthier work environment. Moreover, it's crucial for leaders to recognize the importance of equal opportunities and fair compensation, not only for the well-being of their employees but also for the overall health of the economy.<br />
+
+Interviewer: As a Project Manager, have you witnessed any successful strategies in mitigating this problem?<br />
+
+Intervier (PM): Certainly. Some companies have adopted performance-based reward systems that ensure employees are recognized and rewarded based on their contributions rather than arbitrary factors. Additionally, promoting a culture of open communication and addressing concerns about compensation can go a long way in fostering a fair and inclusive workplace.<br />
+
+Interviewer: Thank you, Mr. Doe, for shedding light on this important issue during the festive season. We appreciate your insights.
+
+Intervier (PM): My pleasure. Thank you for having me.
         </p>
       </div>
     </div>
